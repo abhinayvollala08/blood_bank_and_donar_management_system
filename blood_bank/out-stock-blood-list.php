00748a4775ae26842add1d4330cc26c7b852740e
abhinayvollala08 <95137168+abhinayvollala08@users.noreply.github.com>
2025-05-06 11:37:40 +0530

    Update out-stock-blood-list.php
    Displays blood groups with dangerously low inventory (<5 units) in a clear table format, automatically updating from the blood stock database. Provides instant visibility into urgent supply needs.

--- blood_bank/out-stock-blood-list.php
+++ blood_bank/out-stock-blood-list.php
@@ -9,12 +9,20 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exchange Blood List</title>
+    <title>Blood Stock Management</title>
     <link rel="stylesheet" type="text/css" href="css/style1.css">
     <style>
-        td{
+        td, th {
             width: 200px;
             height: 40px;
+            padding: 8px;
+        }
+        .section {
+            margin-bottom: 40px;
+        }
+        h2 {
+            color: #2c3e50;
+            margin-top: 30px;
         }
     </style>
 </head>
@@ -34,34 +42,68 @@ session_start();
                     header('Location:index.php');
                 }
                 ?>
-                <h1>Out Stock Blood List</h1>
-                <center>
-                    <div id="form">
-                       <table>
-                        <tr>
-                            <td><center><font color=black><b>Name</b></font></center></td>
-                            <td><center><font color=black><b>Mobile Number</b></font></center></td>
-                            <td><center><font color=black><b>Blood Group</b></font></center></td>
-                        </tr>
-                        <?php
-                        // $q=$db->prepare('SELECT * FROM donor_registration');
-                        // $q->fetchAll(PDO::FETCH_ASSOC);
-                        // $r1=$q->execute();
-                        $q=$db->query('SELECT * FROM out_stock_blood');
-                        while($r1=$q->fetch(PDO::FETCH_OBJ)){
-                            ?>
+                
+                <!-- Critically Low Stock Section -->
+                <div class="section">
+                    <h1>Critically Low Blood Stock (Less than 5 units)</h1>
+                    <center>
+                        <div id="form">
+                           <table>
                             <tr>
-                            <td><center><?=$r1->name; ?></center></td>
-                            <td><center><?=$r1->mno; ?></center></td>
-                            <td><center><?=$r1->bname; ?></center></td>
-                        </tr>
-                        <?php
-                        }
-                        ?>
-                        
-                       </table> 
-                    </div>
-                </center>
+                                <th><center>Blood Group</center></th>
+                                <th><center>Available Units</center></th>
+                            </tr>
+                            <?php
+                            $q = $db->query("SELECT * FROM blood_stock WHERE units < 5");
+                            if($q->rowCount() > 0) {
+                                while($r = $q->fetch(PDO::FETCH_OBJ)){
+                                    ?>
+                                    <tr>
+                                    <td><center><?=$r->blood_group; ?></center></td>
+                                    <td><center><?=$r->units; ?></center></td>
+                                </tr>
+                                <?php
+                                }
+                            } else {
+                                echo '<tr><td colspan="2"><center>No blood groups in critical stock</center></td></tr>';
+                            }
+                            ?>
+                           </table> 
+                        </div>
+                    </center>
+                </div>
+
+                <!-- Donation History Section -->
+                <div class="section">
+                    <h2>Donation History</h2>
+                    <center>
+                        <div id="form">
+                           <table>
+                            <tr>
+                                <th><center>Donor Name</center></th>
+                                <th><center>Donation Date</center></th>
+                                <th><center>Blood Group</center></th>
+                            </tr>
+                            <?php
+                            $donationQuery = $db->query("SELECT * FROM donations ORDER BY donation_date DESC");
+                            if($donationQuery->rowCount() > 0) {
+                                while($donation = $donationQuery->fetch(PDO::FETCH_OBJ)){
+                                    ?>
+                                    <tr>
+                                    <td><center><?=$donation->donor_name; ?></center></td>
+                                    <td><center><?=date('d-m-Y', strtotime($donation->donation_date)); ?></center></td>
+                                    <td><center><?=$donation->blood_group; ?></center></td>
+                                </tr>
+                                <?php
+                                }
+                            } else {
+                                echo '<tr><td colspan="3"><center>No donation records found</center></td></tr>';
+                            }
+                            ?>
+                           </table> 
+                        </div>
+                    </center>
+                </div>
             </div>
             <div id="footer">
                 <h4>Copyright</h4>
@@ -70,5 +112,4 @@ session_start();
         </div>
     </div>
 </body>
-
 </html>
